2abfb79bf96e590fb19001aef32ec60525d28635
flpa <flpa.dev@zoho.com>
2014-06-03 19:07:29 +0200

    add clisp format based solution

---
+++ clisp/format.lisp
@@ -0,0 +1,6 @@
+; neat solution i found on the internet
+(loop for i from 1 to 100
+   do (format t "~:[~:[~S~;Buzz~]~;Fizz~:[~;Buzz~]~]~%"
+              (zerop (mod i 3))
+              (zerop (mod i 5))
+              i))
