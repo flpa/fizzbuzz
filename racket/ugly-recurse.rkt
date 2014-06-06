1bb4b99118f12fc31157d1bc9685f3a0e178ab81
flpa <flpa.dev@zoho.com>
2014-06-06 17:28:24 +0200

    ugly recursive solution in racket

---
+++ racket/ugly-recurse.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(define (fizzing to)
+  (unless (zero? to)
+    (fizzing (sub1 to))
+    (if (and (zero? (modulo to 3)) (zero? (modulo to 5)))
+        (printf "FizzBuzz\n")   
+        (if (zero? (modulo to 3))
+            (printf "Fizz\n")
+            (if (zero? (modulo to 5))
+                (printf "Buzz\n")
+                (printf "~a\n" to))))))
